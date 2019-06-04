3539ef21573f131c4f3b33d5a36cd3657827d944
yeming <492851378@qq.com>
2019-06-04 20:19:28 +0800

    initial commit

---
+++ problem1.py
@@ -0,0 +1,29 @@
+"""
+给一个数组，数组内的元素是整数，找出其中重复出现过的整数，返回以这些重复的整数为元素组成的新数组，顺序无所谓。
+例如，给定[4,3,2,7,8,2,3,1,2]，应该返回[2,3]或者[3,2]
+"""
+
+
+def find_dup(items: list) -> list:
+    dup_times_dict = {}  # 纪录每个数字的重复次数
+    dup_nums = []
+
+    # 每遇到一次则 +1
+    for i in items:
+        try:
+            dup_times_dict[i] += 1
+        except: 
+            dup_times_dict[i] = 1
+
+    # 找出重复次数大于 1 的项
+    for k, v in dup_times_dict.items():
+        if v > 1:
+            dup_nums.append(k)
+    
+    return dup_nums
+
+# 不要修改下面的部分
+if __name__ == "__main__":
+    result = find_dup([4, 3, 2, 7, 8, 2, 3, 1, 2])
+    assert result == [2, 3] or result == [3, 2]
+    print("OK")
---
+++ problem2.py
@@ -0,0 +1,18 @@
+"""
+写一个函数将 ipv4 地址字符串 (仅包含数字，点) 转化成 32 位整数，要求输出合法地址的 32 位整型结果。
+"""
+
+def decimal_to_binary():
+
+
+
+def ipv4_to_int(address: str) -> int:
+    pass  # 在这里完成你的代码
+
+
+# 不要修改下面的部分
+if __name__ == "__main__":
+    assert ipv4_to_int("0.0.0.0") == 0
+    assert ipv4_to_int("192.168.0.0") == 3232235520
+    assert ipv4_to_int("255.255.255.255") == 4294967295
+    print("OK")
